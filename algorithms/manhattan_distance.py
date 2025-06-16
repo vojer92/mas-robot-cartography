54d4e5fdbf1953b79d75a119234a4982f737c325
Dominik Schindele <schindele.dominik@gmail.com>
2025-06-16 15:32:26 +0200

    Change heuristics from interface to global utility function

---
+++ algorithms/manhattan_distance.py
@@ -0,0 +1,8 @@
+def calculate_manhattan_distance(
+        pos_a: tuple[int, int],
+        pos_b: tuple[int, int]
+    ) -> float:
+    """
+    Calculate the Manhattan distance between two positions.
+    """
+    return sum(abs(a - b) for a, b in zip(pos_a, pos_b))
