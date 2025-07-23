011ca9db724bc018769d42b8f45ecfa2ff421e4c
Michael Vojer <vojer@outlook.de>
2025-07-23 14:05:32 +0200

    implemented Script for Batchrunner and results with seed 50 uploaded.

--- batch.py
+++ batch.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 from datetime import datetime
 
 import pandas as pd
@@ -10,40 +11,47 @@ MAX_STEPS = 1000
 SEED = 1
 
 if __name__ == "__main__":
-    now = datetime.now()
+    seed_begin = int(sys.argv[1])
+    seed_end = int(sys.argv[2])
+ 
+    for i in range(seed_begin, seed_end+1):
+        now = datetime.now()
 
-    params = {
-        "initial_no_robots": [1, 3, 6, 9],
-        "grid_size": range(40, 101, 20),
-        "robot_type_str": ["RandomWalkRobot", "FBERobot"],
-        "view_radius": range(1, 4, 1),
-        "view_angle": [45, 90, 180, 360],
-        "seed": range(1, 401, 1),
-        "factor_distance": 1.0,
-        "factor_size": [0.0, 0.1, 0.25],
-    }
+        print(f"Starting run with seed {i}")
+        
+        params = {
+            "initial_no_robots": [1, 3, 6, 9],
+            "grid_size": range(40, 101, 20),
+            "robot_type_str": ["RandomWalkRobot", "FBERobot"],
+            "view_radius": range(1, 4, 1),
+            "view_angle": [45, 90, 180, 360],
+            "seed": i,
+            "factor_distance": 1.0,
+            "factor_size": [0.0, 0.1, 0.25],
+        }
 
-    # params = {
-    #     "initial_no_robots": 5,
-    #     "grid_size": 100,
-    #     "robot_type_str": ["RandomWalkRobot", "FBERobot"],
-    #     "view_radius": 1,
-    #     "view_angle": 90,
-    #     "seed": range(1, 51, 1),
-    #     "factor_distance": 0.5,
-    #     "factor_size": 0.5,
-    # }
+        # params = {
+        #     "initial_no_robots": 5,
+        #     "grid_size": 100,
+        #     "robot_type_str": ["RandomWalkRobot", "FBERobot"],
+        #     "view_radius": 1,
+        #     "view_angle": 90,
+        #     "seed": range(1, 2, 1),
+        #     "factor_distance": 0.5,
+        #     "factor_size": 0.5,
+        # }
 
-    results = batch_run(
-        Exploration,
-        parameters=params,
-        iterations=1,
-        max_steps=MAX_STEPS,
-        number_processes=None,
-        data_collection_period=25,
-        display_progress=True,
-    )
+        results = batch_run(
+            Exploration,
+            parameters=params,
+            iterations=1,
+            max_steps=MAX_STEPS,
+            number_processes=None,
+            data_collection_period=25,
+            display_progress=True,
+        )
 
-    df_results = pd.DataFrame.from_dict(results)
-    print(df_results)
-    df_results.to_csv(f"data/Results-{now.strftime("%d.%m.%Y-%H:%M:%S")}.csv", sep=";", index=False)
+        df_results = pd.DataFrame.from_dict(results)
+        df_results.to_csv(f"data/Results-{now.strftime("%d.%m.%Y-%H:%M:%S")}-seed{i}.csv", sep=";", index=False)
+
+        print(f"Run with seed {i} successfull!")
